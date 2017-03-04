fd0a534cbfeb51c0f5d027dadf0959fe35b57c99
Hertzole <hampusringstrom@gmail.com>
2017-03-04 14:41:15 +0100

    Initial commit

--- UnicodeEmoticons/Form1.Designer.cs
+++ UnicodeEmoticons/Form1.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace UnicodeEmoticons
+namespace UnicodeEmoticons
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.discordBox = new System.Windows.Forms.CheckBox();
+            this.buttonPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // discordBox
+            // 
+            this.discordBox.AutoSize = true;
+            this.discordBox.Location = new System.Drawing.Point(12, 12);
+            this.discordBox.Name = "discordBox";
+            this.discordBox.Size = new System.Drawing.Size(92, 17);
+            this.discordBox.TabIndex = 0;
+            this.discordBox.Text = "Discord Mode";
+            this.discordBox.UseVisualStyleBackColor = true;
+            // 
+            // buttonPanel
+            // 
+            this.buttonPanel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPanel.AutoScroll = true;
+            this.buttonPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.buttonPanel.Location = new System.Drawing.Point(12, 35);
+            this.buttonPanel.Name = "buttonPanel";
+            this.buttonPanel.Size = new System.Drawing.Size(280, 314);
+            this.buttonPanel.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(304, 361);
+            this.Controls.Add(this.buttonPanel);
+            this.Controls.Add(this.discordBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "MainForm";
+            this.Text = "Unicode Emoticons";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox discordBox;
+        private System.Windows.Forms.FlowLayoutPanel buttonPanel;
     }
 }
 
--- UnicodeEmoticons/Form1.cs
+++ UnicodeEmoticons/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,11 +10,52 @@ using System.Windows.Forms;
 
 namespace UnicodeEmoticons
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        private string[] emoticons = new string[] { "( ͡° ͜ʖ ͡°)", "( ͡~ ͜ʖ ͡°)", "( ͠° ͜ʖ ͡°)", "( ͡o ͜ʖ ͡o)", "( ͠° ͟ʖ ͡°) ", "¯\\_(ツ)_/¯", "¯\\_(°_o)_/¯", "ಠ_ಠ", "ಠ‿ಠ", "◕‿◕", "(⌐■_■)", "(╯°□°）╯︵ ┻━┻", "(ノಠ益ಠ)ノ彡┻━┻", "┻━┻ ︵ヽ(`Д´)ﾉ︵ ┻━┻", "（╯°□°）╯︵(\\ .o.)\\", "┬──┬ ノ( ゜-゜ノ)", "(∩◕ᴥ◕)⊃━☆ﾟ.*", "(∩ಠ‿ಠ)⊃━☆ﾟ.*", "ヽ༼ຈل͜ຈ༽ﾉ", "( ﾟヮﾟ)", "(´・ω・)っ由" };
+
+        public MainForm()
         {
             InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < emoticons.Length; i++)
+            {
+                Button newButton = new Button()
+                {
+                    MinimumSize = new Size(123, 25),
+                    Text = emoticons[i],
+                    Font = new Font(FontFamily.GenericSerif, 8),
+                };
+
+                newButton.Tag = emoticons[i];
+                newButton.Click += ButtonClick;
+
+                buttonPanel.Controls.Add(newButton);
+            }
+        }
+
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            string emoji = (sender as Button).Tag.ToString();
+
+            emoji = Convert(emoji);
+
+            Clipboard.SetText(emoji);
+        }
+
+        private string Convert(string text)
+        {
+            bool discordMode = discordBox.Checked;
+
+            if (discordMode)
+            {
+                text = text.Replace("_", "\\_");
+            }
+
+            return text;
         }
     }
 }
